5562be039c632bbe5f3faacea10e10447ad69a38
Anton <aglanton31@gmail.com>
2020-01-28 18:13:26 +0300

    contacts

--- servicePolo/php/form.php
+++ servicePolo/php/form.php
@@ -12,17 +12,17 @@
       padding: 0 50px;
     }
     h1 {
-      color: #006d79;
+      color: #25527D;
       font-size: 50px;
     }
-    h2, ul {
-      text-align: left;
+    p {
+      font-size: 1.3rem;
+      padding-bottom: 3rem;
     }
     .button {
-      background-color: #006d79;
+      background-color: #25527D;
       padding: 10px 20px;
       border-radius: 15px;
-      text-transform: uppercase;
       color: #fff;
       font-size: 25px;
       transition: .3s;
@@ -30,7 +30,7 @@
       text-decoration: none;
     }
     .button:hover {
-      background-color: #00545f;
+      background-color: rgb(32, 72, 109);
       text-decoration: none;
       color: #fff;
     }
@@ -43,161 +43,63 @@
 <body>
 
 <?php
-  // get data
-  $name = $_POST["name"];
-  // $email = $_POST["email"];
-  $number = $_POST["number"];
-  $vincode = strtoupper( $_POST["vincode"] );
-  $comment = $_POST["comment"];
-
-  //spare parts
-  $filter = $_POST["filter"];
-  $filterMessage = '';
-  if (!empty($filter)) {
-    $filterMessage = "<li><b>Фильтра:</b></li>";
-    for ($i = 0; $i < count($filter); $i++) {
-      $filterMessage .= ($i+1 . " - " . $filter[$i] . "<br>");
-    }
-    $filterMessage .= "<br>";
-  }
-
-  $oil = $_POST["oil"];
-  $oilMessage = '';
-  if (!empty($oil)) {
-    $oilMessage = "<li><b>Масла:</b></li>";
-    for ($i = 0; $i < count($oil); $i++) {
-      $oilMessage .= ($i+1 . " - " . $oil[$i] . "<br>");
-    }
-    $oilMessage .= "<br>";
-  }
-
-  $brake = $_POST["brake"];
-  $brakeMessage = '';
-  if (!empty($brake)) {
-    $brakeMessage = "<li><b>Тормоза:</b></li>";
-    for ($i = 0; $i < count($brake); $i++) {
-      $brakeMessage .= ($i+1 . " - " . $brake[$i] . "<br>");
-    }
-    $brakeMessage .= "<br>";
-  }
   
-  $liquid = $_POST["liquid"];
-  $liquidMessage = '';
-  if (!empty($liquid)) {
-    $liquidMessage = "<li><b>Жидкости:</b></li>";
-    for ($i = 0; $i < count($liquid); $i++) {
-      $liquidMessage .= ($i+1 . " - " . $liquid[$i] . "<br>");
-    }
-    $liquidMessage .= "<br>";
-  }
+  $name = $_POST['name'];
+  $number = $_POST['number'];
+  $email = $_POST['email'] ? $_POST['email'] : ' -';
+  $vincode = $_POST['vincode'] ? $_POST['email'] : ' -';
+    
+  $pack1 = $_POST['pack1'];
+  $pack2 = $_POST['pack2'];
+  $pack3 = $_POST['pack3'];
+  $pack = ($pack1 || $pack2 || $pack3) ? ($pack1 . ' ' . $pack2 . ' ' . $pack3) : ' -';
+  
+  $comment = $_POST['comment'];
 
-  if (!empty($name) ||
-      !empty($number) ||
-      !empty($vincode) ) {
-          
-      // bd
-      // Параметры для подключения
-      $db_host = "h907188693.mysql"; 
-      $db_user = "h907188693_mysql"; // Логин БД
-      $db_password = "QU+KsKW1"; // Пароль БД
-      $db_base = 'h907188693_db'; // Имя БД
-      $db_table = "Users"; // Имя Таблицы БД
-    
-      // Подключение к базе данных
-      $mysqli = new mysqli($db_host,$db_user,$db_password,$db_base);
-    
-      // Если есть ошибка соединения, выводим её и убиваем подключение
-      if ($mysqli->connect_error) {
-          die('Ошибка : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
-      }
-      
-      $spareParts = '';
-      if (!empty($filter)) {
-        for ($i = 0; $i < count($filter); $i++) {
-          $spareParts .= $filter[$i] . '\n';
+  // compilation letter
+  if ( !empty($name) && !empty(number) ) {
+    $to = "servicepolo@ya.ru";
+    $subject = "Service polo";
+
+    $message = 
+      "<style>
+        ul {
+          list-style:none;
+          margin: 0;
+          padding: 0;
         }
-      }
-      if (!empty($oil)) {
-        for ($i = 0; $i < count($oil); $i++) {
-          $spareParts .= $oil[$i] . '\n';
+        li {
+          padding: 4px 0;
         }
-      }
-      if (!empty($brake)) {
-        for ($i = 0; $i < count($brake); $i++) {
-          $spareParts .= $brake[$i] . '\n';
-        }
-      }
-      if (!empty($liquid)) {
-        for ($i = 0; $i < count($liquid); $i++) {
-          $spareParts .= $liquid[$i] . '\n';
-        }
-      }
-      
-      $result = $mysqli->query("INSERT INTO ".$db_table."
-      (`name`, `number`, `vin code`, `comment`, `spare parts`) VALUES
-      ('$name', '$number', '$vincode', '$comment', '$spareParts')");
-    
-      // compilation letter
-      // $to = "aglanton31@ya.ru";
-      $to = "vw-to@ya.ru";
-      $subject = "VW-TO";
-    
-      $message = 
-        "<style>
-          ul {
-            list-style:none;
-            margin: 0;
-            padding: 0;
-          }
-          li {
-            padding: 4px 0;
-          }
-        </style>" .
-        "<ul>" .
-        "<li><b>Имя:</b> $name</li>" .
-        "<li><b>Номер:</b> $number</li>" .
-        "<li><b>Vin код:</b> $vincode</li><br>" .
-        $filterMessage .
-        $oilMessage .
-        $brakeMessage .
-        $liquidMessage;
-    
-      if (!empty($comment)) {
-        $comment = nl2br($comment);
-        $message .= "<li><b>Комментарий:</b></li> $comment";
-      }
-    
-      $message .= "</ul>";
-    
-      $headers  = "Content-type: text/html; charset=UTF-8 \r\n";
-    
-      // send letter
-      if ( mail ($to, $subject, $message, $headers) ) {
-        $answerMessage = 
-          "<h1>Спасибо за заказ!</h1>" .
-          "<p>Скоро к вам на почту придут цены</p>" .
-          "<h2>Ваши данные:</h2>" .
-          $message;
-        echo $answerMessage;
-        
-        // mail ($email, "Спасибо за заказ!", $answerMessage, $headers);
-        
-      } else {
-        echo "
-          <h1>Упс, что-то пошло не так!</h1>
-          <p>Попробуте отправить форму ещё раз</p>
-          <button class='button repeat' onclick='window.location.reload()'>Повторить</button>
-          <h2>Ваши данные:</h2>
-          $message
-        ";
-      }
+      </style>" .
+      "<h1>Заявка обслужиполо.рф</h1>" .
+      "<ul>" .
+      "<li><b>Имя:</b> $name</li>" .
+      "<li><b>Номер:</b> $number</li>" .
+      "<li><b>E-mail:</b> $email</li>" .
+      "<li><b>Vin код:</b> $vincode</li><br>" .
+      "<li><b>Пакеты:</b> $pack</li><br>";
+
+    if (!empty($comment)) {
+      $comment = nl2br($comment);
+      $message .= "<li><b>Комментарий:</b></li> $comment";
+    }
+
+    $message .= "</ul>";
+
+    $headers  = "Content-type: text/html; charset=UTF-8 \r\n";
+
+    // send letter
+    if ( mail ($to, $subject, $message, $headers) ) {
+        echo '<h1>Спасибо за заявку!</h1>' .
+            '<p>Мы Вам перезвоним</p>'; 
+    } else {
+      echo '<h1>Упс, что-то пошло не так!</h1>' .
+      '<p>Повторите попытку или позвоните нам: <a href="tel:+79891250220">+7 (989) 125-02-20</a></p>'; 
+    }
   } else {
-      echo "
-        <h1>Упс, что-то пошло не так!</h1>
-        <p>Попробуте отправить форму ещё раз</p>
-        <button class='button repeat' onclick='window.location.reload()'>Повторить</button>
-        <h2>Ваши данные:</h2>
-      ";
+    echo '<h1>Упс, что-то пошло не так!</h1>' .
+    '<p>Повторите попытку или позвоните нам: <a href="tel:+79891250220">+7 (989) 125-02-20</a></p>'; 
   }
 ?>
 
