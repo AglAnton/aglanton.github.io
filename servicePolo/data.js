ab9a93b50fce67ebb3700cb459b369f2157705c8
Антон Аглушевич <aglanton31@gmail.com>
2023-02-07 12:29:18 +0300

    up

---
+++ servicePolo/data.js
@@ -0,0 +1,587 @@
+var pricesData = {
+  'Polo SD 1.6 2010-2015': [
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_2.jpg',
+      name: 'Пакет 2',
+      desc: 'Работа + материалы',
+      price: 3000,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 450,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра',
+            'new'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    }
+  ],
+  'Polo SD, NF 1.6 2015-Н.В.': [
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    }
+  ],
+  'Polo SD 1.4 2015-2020': [
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    }
+  ],
+  'Polo NF 1.4 2020-Н.В.': [
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    },
+    {
+      img: 'pack_1.jpg',
+      name: 'Пакет 1',
+      desc: 'Работа + материалы',
+      price: 3200,
+      images: [
+        {
+          path: 'oil.jpg',
+          alt: 'Масло'
+        },
+        {
+          path: 'oil_filter.jpg',
+          alt: 'Масляный фильтр'
+        },
+        {
+          path: 'stopper.jpg',
+          alt: 'Пробка'
+        }
+      ],
+      services: [
+        {
+          name: 'G 052502M9 − Масло 4л. (ориг. разливное)',
+          price: 2070,
+          more: []
+        },
+        {
+          name: '03C115561H − Масляный фильтр',
+          price: 614,
+          more: []
+        },
+        {
+          name: 'N 90813202 − Пробка',
+          price: 121,
+          more: []
+        },
+        {
+          name: 'Работа',
+          price: 400,
+          more: [
+            'Замена масла',
+            'Замена масляного фильтра'
+          ]
+        }
+      ]
+    }
+  ]
+}
--- servicePolo/script/script.js
+++ servicePolo/script/script.js
@@ -1,6 +1,102 @@
 let lab;
 $(document).ready(()=>{
 
+  let $tabsPrice = $('#price .tabs');
+  let i = 0;
+  for (let name in pricesData) {
+    $tabsPrice.find('.tabs__menu').append(`
+      <div class="tabs__item ${i==0 ? 'active' : ''}">
+        ${name}
+      </div>
+    `);
+    $tabsPrice.find('.tabs__content').append(`
+      <div class="tabs__content-item ${i==0 ? 'active wow fadeInLeft' : ''}">
+        <div class="row">
+        </div>
+      </div>
+    `);
+
+    pricesData[name].forEach(pack => {
+      $tabsPrice.find('.tabs__content-item .row').last().append(`
+      <div class="col-lg-4 mb-5">
+        <div class="price-block">
+          <img src="img/price/${pack.img}">
+
+          <h4>${pack.name}</h4>
+          <p class="description">${pack.desc}</p>
+          <p class="price-our">${pack.price}&#8381;</p>
+
+          <button type="button" data-toggle="modal" data-target="#pack" class="button" data-pack='${JSON.stringify(pack)}' data-model="${name}">Подробнее</button>
+        </div>
+      </div>
+      `);
+    });
+    i++;
+  }
+
+  $('button[data-toggle="modal"][data-target="#pack"]').on('click', function() {
+    let $modal = $('#pack');
+    let pack = $(this).data('pack');
+    console.log(pack);
+
+    $modal.find('.modal-title').text(pack.name);
+
+    $modal.find('.carousel-indicators').html('');
+    $modal.find('.carousel-inner').html('');
+    pack.images.forEach((img, i) => {
+      $modal.find('.carousel-indicators').append(`
+      <li data-target="#pack-slider" data-slide-to="${i}" ${i==0 ? 'class="active"' : ''}></li>
+      `);
+      
+      $modal.find('.carousel-inner').append(`
+        <div class="carousel-item ${i==0 ? 'active' : ''}">
+          <img class="modal-img d-block w-100" src="img/price/${img.path}" alt="${img.alt}">
+          <div class="carousel-caption">
+            <p class="description modal-description">${img.alt}</p>
+          </div>
+        </div>
+      `);
+    });
+
+    $modal.find('.modal-list').html('');
+    pack.services.forEach(service => {
+      let moreHtml = '';
+      if (service.more.length > 0) {
+        moreHtml += '<ul class="modal-sublist">';
+        service.more.forEach(item => {
+          moreHtml += `<li class="modal-sublist_elem">${item}</lic>`
+        });
+        moreHtml += '</ul>';
+      }
+
+      $modal.find('.modal-list').append(`
+        <li>
+          <div class="row">
+            <div class="col-8">${service.name} ${moreHtml}</div>
+            <div class="col-4">
+              <div class="modal-price">- ${service.price}&#8381;</div>
+            </div>
+          </div>
+        </li>
+      `);
+    });
+
+    $modal.find('.modal-total-price span').text(pack.price);
+
+    $modal.find('.modal-button').data('pack', pack.name);
+    $modal.find('.modal-button').data('model', $(this).data('model'));
+  });
+
+  $('.modal-button').on('click', function() {
+    let newVal = 'Мне нужен ' + $(this).data('pack') + ' для ' + $(this).data('model');
+    let valComment = $('#comment').val();
+    if (valComment.trim() !== '') {
+      valComment += "\r\n";
+    }
+    $('#comment').val(valComment + newVal);
+  });
+
+
   // menu scroll
   $(window).scroll(()=>{
     menuScroll();
@@ -82,6 +178,19 @@ $(document).ready(()=>{
   }
   stagesAdaptive();
 
+
+  // tabs 
+  $('.tabs__item').on('click', function() {
+    let $tabs = $(this).closest('.tabs');
+    $tabs.find('.tabs__item').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).index();
+    $tabs.find('.tabs__content-item').removeClass('active')
+    $tabs.find('.tabs__content-item').eq(index).addClass('active');
+  });
+
+  animate();
 });
 
 // adaptation animate
@@ -100,4 +209,3 @@ function animate() {
     $('[data-wow-delay]').removeAttr('data-wow-delay');
   }
 }
-animate();
